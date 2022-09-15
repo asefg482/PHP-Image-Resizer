9a861af6d7473e902a223743a5955eb9abee4e8a
Asef <asefg482@gmail.com>
2022-09-15 17:26:17 +0430

    First

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+$Dir = scandir('Files/');
+unset($Dir[0]);
+unset($Dir[1]);
+$Dir = array_values($Dir);
+
+
+function resize_image($file,$w,$h,$crop = FALSE)
+{
+	list($width,$height) = getimagesize($file);
+	$r = $width / $height;
+	if ($crop)
+	{
+		if ($width > $height)
+		{
+			$width = ceil($width - ($width * abs($r - $w / $h)));
+		}
+		else
+		{
+			$height = ceil($height - ($height * abs($r - $w / $h)));
+		}
+		$newwidth = $w;
+		$newheight = $h;
+	}
+	else
+	{
+		if ($w / $h > $r)
+		{
+			$newwidth = $h * $r;
+			$newheight = $h;
+		}
+		else
+		{
+			$newheight = $w / $r;
+			$newwidth = $w;
+		}
+	}
+	$src = imagecreatefromjpeg($file);
+	$dst = imagecreatetruecolor($newwidth,$newheight);
+	imagecopyresampled($dst,$src,0,0,0,0,$newwidth,$newheight,$width,$height);
+
+	return $dst;
+}
+
+
+foreach ($Dir as $Key => $Item)
+{
+	//1000 - 450
+	$img = resize_image('Files/' . $Item,1200,540);
+	//	$img = resize_image('Files/'.$Item, 1000, 450);
+	imagejpeg($img,'Resized/Gun_' . $Key . '.jpg');
+}
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+foreach (range(0,160) as $Item)
+{
+	Echo " {
+    \"Gun_Name\": '',
+    \"Image_Asset\":'Assets/Images/Shots/Gun_$Item.jpg',
+    \"For\": '',
+    \"Level\": '',
+    \"Type\": '',
+    \"Gun_Type\": '',
+  },";
+}
